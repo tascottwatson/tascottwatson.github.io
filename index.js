9da8ef6318b0edd0ca0edc77b158fd57d59f3eed
tascottwatson <47367118+tascottwatson@users.noreply.github.com>
2020-01-05 20:53:37 +0000

    new update cp3

---
+++ index.js
@@ -0,0 +1,23 @@
+var bgImageArray = ["lamarcus.jpg", "paulgeorge.jpg", "anthony.jpg"],
+base = "",
+secs = 6;
+
+bgImageArray.forEach(function(img){
+    new Image().src = base + img;
+});
+
+function backgroundSequence() {
+window.clearTimeout();
+	var k = 0;
+	for (var i = 0; i < bgImageArray.length; i++) {
+		setTimeout(function(){
+document.documentElement.style.background = "url(" + base + bgImageArray[k] + ") no-repeat center center fixed";
+document.documentElement.style.backgroundSize ="cover";
+			if ((k + 1) === bgImageArray.length) {
+				setTimeout(function() {
+					backgroundSequence() }, (secs * 1000))
+			} else { k++; }
+		}, (secs * 1000) * i)
+	}
+}
+backgroundSequence();
